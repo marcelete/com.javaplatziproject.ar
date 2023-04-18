aab2e453bab48e8fe328ad2f3a1e69ea82f404a1
Marcelo Jorge Bellizia <marcebellizia@gmail.com>
2023-04-18 09:47:47 -0300

    Creando interfaz ISchedulable

--- src/Model/AppointmentDoctor.java
+++ src/Model/AppointmentDoctor.java
@@ -10,4 +10,43 @@ public class AppointmentDoctor {
     private Patient patient;
     private Doctor doctor;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
--- src/Model/AppointmentNurse.java
+++ src/Model/AppointmentNurse.java
@@ -10,4 +10,43 @@ public class AppointmentNurse {
     private Patient patient;
     private Nurse nurse;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Nurse getNurse() {
+        return nurse;
+    }
+
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
+    }
 }
